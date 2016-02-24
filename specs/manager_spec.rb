e30f910ff7434dc2e4b35f1965ffbb26c794eadb
guy meikle <left_handed_rocker@hotmail.co.uk>
2016-02-24 12:31:02 +0000

    manager test1 complete

--- specs/manager_spec.rb
+++ specs/manager_spec.rb
@@ -1,12 +1,19 @@
 require ('minitest/autorun')
-require_relative('team')
-require_relative('player')
-require_relative('bench')
+require_relative('../team')
+require_relative('../player')
+require_relative('../bench')
+require_relative('../manager')
 
 class TestManager < Minitest::Test
 
+def setup
+  @manager = Manager.new("Bon Jovi", "Man Utd", "Barca", "Alloa")  
+end
 
-
+def test_coach_name
+  assert_equal("Bon Jovi", @manager.name)
+  
+end
 
 
 
