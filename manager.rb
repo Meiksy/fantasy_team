9df9cdd9d6bc88a9bc5e50278ca4dce872e26b20
guy meikle <left_handed_rocker@hotmail.co.uk>
2016-02-24 13:33:36 +0000

    manager talking to team

--- manager.rb
+++ manager.rb
@@ -2,14 +2,18 @@ class Manager
 
 attr_reader(:name, :previous_clubs)
 
-def initialize(name, previous_clubs)
+def initialize(name, *previous_clubs)
   @name = name
   @previous_clubs = previous_clubs
   @salary = 500000
 end
 
-
-
+def add_players_to_team(team, *players)
+  for player in players
+    team.players.push(player)
+  end
+  
+end
 
 
 
--- specs/manager_spec.rb
+++ specs/manager_spec.rb
@@ -8,14 +8,21 @@ class TestManager < Minitest::Test
 
 def setup
   @manager = Manager.new("Bon Jovi", "Man Utd", "Barca", "Alloa")  
+  @team1 = Team.new("CodeClan Utd", )
 end
+
+
 
 def test_coach_name
   assert_equal("Bon Jovi", @manager.name)
   
 end
 
-
+def test_manager_add_players
+  result = @manager.add_players_to_team(@team1, "Bob", "Steve", "Trev")
+  assert_equal(["Bob", "Steve", "Trev"], @team1.players)
+  
+end
 
 
 end
